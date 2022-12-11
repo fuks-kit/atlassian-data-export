e77336ba96ab8e934b062772c8c1d50a087c44b1
Patrick Zierahn <5384625+pzierahn@users.noreply.github.com>
2022-12-11 17:54:36 +0100

    Added exportDir arg

---
+++ cmd/confluence/confluence.export.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"flag"
+	"github.com/fuks-kit/atlassian-data-export/confluence"
+	"log"
+)
+
+var (
+	baseUrl   = flag.String("url", "http://localhost:8090", "confluence base url")
+	username  = flag.String("username", "", "confluence username")
+	password  = flag.String("password", "", "confluence password")
+	exportDir = flag.String("exportDir", "export", "export directory")
+)
+
+func main() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	flag.Parse()
+
+	downloader := confluence.Downloader{
+		BaseUrl:  *baseUrl,
+		Username: *username,
+		Password: *password,
+	}
+
+	downloader.Download(*exportDir)
+}
